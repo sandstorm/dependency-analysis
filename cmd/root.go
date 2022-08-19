a670edad22b9105980a99b836c0fd716317e88de
Christoph Dähne <christoph.daehne@sandstorm.de>
2022-08-19 09:53:34 +0200

    TASK: disable not working auto completion

--- cmd/root.go
+++ cmd/root.go
@@ -27,12 +27,7 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	// rootCmd.PersistentFlags().String("source", "", "directory to read source files from (defaults to the current directory)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// does not work out of the box
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.CompletionOptions.DisableDescriptions = true
 }
