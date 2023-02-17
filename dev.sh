3757d36444f87c15c81ff39d02b257de5d3cc734
Christoph Dähne <christoph.daehne@sandstorm.de>
2023-02-17 13:48:59 +0100

    TASK: fine-tuning of new release mechanism

--- dev.sh
+++ dev.sh
@@ -14,6 +14,11 @@ set -e
 
 ######### TASKS #########
 
+# Starts the project from source
+function run() {
+  go run . $@
+}
+
 # Compiles the project into a local binary
 function build() {
   go build .
@@ -42,9 +47,21 @@ function test() {
 # Builds the project locall and creates a release
 function release() {
   test
+  docker build -t sandstormmedia/dependency-analysis:latest .
   goreleaser release --clean
   _log_green "done"
-  _log_yellow "TODO: implement docker image releases"
+  _log_yellow "Manual steps:"
+  _log_yellow " - docker tag sandstormmedia/dependency-analysis:latest sandstormmedia/dependency-analysis:version-goes-here"
+  _log_yellow " - docker push sandstormmedia/dependency-analysis:latest"
+  _log_yellow " - docker push sandstormmedia/dependency-analysis:the-version"
+}
+
+# Docs in the browser at localhost:8080
+function docs() {
+  which godoc || go install golang.org/x/tools/cmd/godoc@latest
+  _log_green "Please open http://localhost:8080/pkg/github.com/sandstorm/dependency-analysis/"
+  _log_green "Terminate with Ctrl + C"
+  godoc -http=:8080
 }
 
 _log_green "---------------------------- RUNNING TASK: $1 ----------------------------"
